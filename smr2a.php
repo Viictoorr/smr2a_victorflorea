e2fa654d5f64b11d827f5198ff0254b3d314fa8b
VictorFlorea <floreavictor0@gmail.com>
2019-10-01 19:44:19 +0200

    Timestamp

--- smr2a.php
+++ smr2a.php
@@ -1,9 +1,12 @@
+<a href="http://192.168.0.173/smr2a-VCR/smr2a.php">Victor Cabo</a>
+
 Victor Florea ¯\_(ツ)_/¯
 
 <?php
 $f=fopen('visitas.txt','a');
+fwrite($f,date('Y-m-d H:i:s '));
 fwrite($f,$_SERVER['REMOTE_ADDR']."\r\n");
 fclose($f)
 ?>
 
-<a href="http://192.168.0.104/smr2a_ADRIAN/smr2a.php">Adri</a>
+<a href="http://192.168.0.104/smr2a-Gabriel/smr2a.php">Gabi</a>
