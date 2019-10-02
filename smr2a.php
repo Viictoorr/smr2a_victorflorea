7c83daa22bb14e297a875da41e483fbb26a33c3e
VictorFlorea <floreavictor0@gmail.com>
2019-10-02 15:46:45 +0200

    Formulario

--- smr2a.php
+++ smr2a.php
@@ -1,6 +1,28 @@
+<?php
+if(isset($_GET['Correo'])){
+	$f=fopen('Suscriptores.txt','a');
+	fwrite($f,$_GET['Correo']."\r\n");
+	fclose($f);
+}
+?>
+
+<html>
+<body>
+
 <a href="http://192.168.0.173/smr2a-VCR/smr2a.php">Victor Cabo</a>
 
 Victor Florea ¯\_(ツ)_/¯
+
+<a href="http://192.168.0.104/smr2a-Gabriel/smr2a.php">Gabi</a>
+
+<form>
+<center>Email:<center>
+<center><input type="email" name="correo"><br><center>
+<center><button>Enviar</button><center>
+</form>
+
+</body>
+</html>
 
 <?php
 $f=fopen('visitas.txt','a');
@@ -8,5 +30,3 @@ fwrite($f,date('Y-m-d H:i:s '));
 fwrite($f,$_SERVER['REMOTE_ADDR']."\r\n");
 fclose($f)
 ?>
-
-<a href="http://192.168.0.104/smr2a-Gabriel/smr2a.php">Gabi</a>
