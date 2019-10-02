21dc0876bf31428fcfa5170c3e6111f6bd1337b0
VictorFlorea <floreavictor0@gmail.com>
2019-10-02 16:26:53 +0200

    Catalogo

---
+++ Catalogo.php
@@ -0,0 +1,13 @@
+<hrml>
+<h1>
+<a href="http://192.168.0.72/smr2a_VictorFlorea/smr2a.php"> <---</a><center>Listado GitHub<center>
+</h1>
+	<center><p><a href="https://github.com/gabrieldecroly" target="_blank">Gabi</a></p>
+	<p><a href="https://github.com/jhoss11" target="_blank">Jhostin</a></p>
+	<p><a href="https://github.com/pablodecroly" target="_blank">Pablo</a></p>
+	<p><a href="https://github.com/victorflorea" target="_blank">Victor Florea</a></p>
+	<p><a href="https://github.com/anabelladecroly" target="_blank">Anabella</a></p>
+	<p><a href="https://github.com/dblancoformacion" target="_blank">David Profe</a></p>
+	<p><a href="https://github.com/VictorCaboRamirez" target="_blank">Victor Cabo</a></p>
+	<p><a href="https://github.com/adriiiann" target="_blank">Adri</a></p><center>
+</html>
--- smr2a.php
+++ smr2a.php
@@ -1,32 +1,35 @@
 <?php
-if(isset($_GET['Correo'])){
+if(isset($_GET['correo'])){
 	$f=fopen('Suscriptores.txt','a');
-	fwrite($f,$_GET['Correo']."\r\n");
+	fwrite($f,$_GET['correo']."\r\n");
 	fclose($f);
 }
 ?>
 
 <html>
-<body>
+	<h1>
+	<center><a href="http://localhost/smr2a_VictorFlorea/catalogo.php">Lista GitHub</a><center>
+	</h1>
+	</html>
 
+<html>
+	<body>
+	<form>
+	<center>Email:<center>
+	<center><input type="email" name="correo"><br><center>
+	<center><button>Enviar</button><center>
+	</form>
+	</body>
+</html>
+
+<?php
+$f=fopen('visitas.txt','a');
+	fwrite($f,date('Y-m-d H:i:s '));
+	fwrite($f,$_SERVER['REMOTE_ADDR']."\r\n");
+	fclose($f)
+?>
 <a href="http://192.168.0.173/smr2a-VCR/smr2a.php">Victor Cabo</a>
 
 Victor Florea ¯\_(ツ)_/¯
 
 <a href="http://192.168.0.104/smr2a-Gabriel/smr2a.php">Gabi</a>
-
-<form>
-<center>Email:<center>
-<center><input type="email" name="correo"><br><center>
-<center><button>Enviar</button><center>
-</form>
-
-</body>
-</html>
-
-<?php
-$f=fopen('visitas.txt','a');
-fwrite($f,date('Y-m-d H:i:s '));
-fwrite($f,$_SERVER['REMOTE_ADDR']."\r\n");
-fclose($f)
-?>
