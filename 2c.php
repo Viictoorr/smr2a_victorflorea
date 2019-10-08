6c485f366c32239af22af5fb61a3f56be16a51d0
VictorFlorea <floreavictor0@gmail.com>
2019-10-08 20:27:18 +0200

    Concatenaciones

--- 2c.php
+++ 2c.php
@@ -1,10 +1,18 @@
 <?php
-function calcula($a,$b){
-
-	echo ($a+$b).'<br>';
-	echo ($a-$b).'<br>';
-	echo ($a*$b).'<br>';
-	echo ($a/$b).'<br>';
+function calcular(){
+	$a=23;
+	$b=43;
+	$r=null;
+	
+	$r=$r.($a+$b).'<br>';
+	$r=$r.($a-$b).'<br>';
+	$r=$r.($a*$b).'<br>';
+	$r=$r.($a/$b).'<br>';
+	return $r;
 }
-calcula(4,7);
+echo calcular();
+$a=calcular();
+echo $a;
+calcular();
+calcular();
 ?>
