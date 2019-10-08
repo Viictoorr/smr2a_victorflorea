d3e7bb30474abd2755d9bfc74563f21d3176397a
VictorFlorea <floreavictor0@gmail.com>
2019-10-08 20:05:32 +0200

    Funciones

---
+++ 2b.php
@@ -0,0 +1,18 @@
+<?php
+function calcula(){
+	$a=23;
+	$b=43;
+
+	echo ($a+$b).'<br>';
+	echo ($a-$b).'<br>';
+	echo ($a*$b).'<br>';
+	echo ($a/$b).'<br>';
+}
+calcula ();
+calcula ();
+calcula ();
+calcula ();
+calcula ();
+calcula ();
+calcula ();
+?>
